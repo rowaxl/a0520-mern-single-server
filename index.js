762fd8d862ef823532daa54dfce5b678f572d793
Francois Polo <andasan@gmail.com>
2021-01-26 15:05:26 -0800

    rectify app.use

--- index.js
+++ index.js
@@ -22,11 +22,14 @@ const authRoutes = require('./routes/auth.route');
 
 app.use('/api/auth', authRoutes);
 
+
 if(process.env.NODE_ENV === 'production'){
+    //express will serve up production assets
     app.use(express.static('client/build'));
 
+    //express will serve up the index.html file if it doesnt recognize the route
     const path = require('path');
-    app('*', (req,res) => {
+    app.use('*', (req,res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     })
 }
